038474223b92422e3ce6f9e37abf42f319d1768b
Matthew DeMichele <mdemichele@opentechalliance.com>
2024-06-06 14:14:17 -0700

    adds initial markup for workspace page

---
+++ src/routes/workspace/+page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import * as Table from '$lib/components/ui/table';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import ArrowLeft from 'lucide-svelte/icons/arrow-left';
+
+	let { data } = $props();
+</script>
+
+<div class="mx-auto mt-8 max-w-3xl px-4 sm:px-6 lg:px-8">
+	<!-- header nav -->
+	<div class="">
+		<Button href="/">
+			<ArrowLeft class="mr-2 h-4 w-4"></ArrowLeft>
+			Back
+		</Button>
+	</div>
+
+	<!-- Title section -->
+	<div class="mx-auto mt-8 max-w-3xl px-4 sm:px-6 lg:px-8">
+		<h2 class="scroll-m-20 text-4xl font-bold tracking-tight lg:text-5xl">Add Workspace</h2>
+	</div>
+
+	<!-- Form section -->
+	<div class="space-y-6 rounded-md border p-4"></div>
+</div>
